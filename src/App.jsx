e7cb1ff54e102adb55c60d21d8a35e91ecff453e
insharahkalam <insharahkalam19@gmail.com>
2026-01-22 10:39:46 +0500

    All done

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Footer from './Footer'
 
 const App = () => {
   return (
-    <div>
+    <div className='overflow-x-hidden'>
       <Navbar />
       <Hero />
       <About />
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
 
     return (
         <nav className="fixed top-0 left-0 w-full z-50 bg-black/90 backdrop-blur border-b border-gray-800">
-            <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+            <div className=" px-6 py-4 flex items-center justify-between">
 
                 {/* LOGO */}
                 <h3 className="text-3xl font-extrabold text-red-700 tracking-wide cursor-pointer">
